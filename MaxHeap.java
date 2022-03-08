692279ff6369ed9b5ec4894257688f8015aa3629
Tyler Norby <tyler.norby@live.com>
2022-03-08 09:52:51 -0700

    Lab 02 Finished, pre testing

--- MaxHeap.java
+++ MaxHeap.java
@@ -3,86 +3,120 @@ public class MaxHeap<T extends Comparable<T>> {
     private T[] heap;
     private int heapSize;
     private int capacity;
-    
-    public MaxHeap(int capacity){
+
+    public MaxHeap(int capacity) {
         this.capacity = capacity;
-        heap = (T[])new Comparable[capacity];
+        heap = (T[]) new Comparable[capacity];
         heapSize = 0;
     }
+
+    static int left(int i){
+        return 2 * i + 1;
+    }
+
+    static int right(int i){
+        return 2 * i + 2;
+    }
+
+    static int parent(int i){
+        return (i-1)/2;
+    }
+
+    void swap(int i, int p){
+        T temp;
+        temp = heap[i];
+        heap[i] = heap[p];
+        heap[p] = temp;
+
+    }
+
     //TODO: This might be 0
-    public T extractHeapMax(){
+    public T extractHeapMax() {
+        //javac *.java
+        //./run_test 0.5
         T popped = heap[1];
         heap[1] = heap[heapSize--];
         maxHeapify(1);
         return popped;
     }
-    //TODO: This might be 0
-    public void maxHeapInsert(T o){
-        heap[heapSize] = o;
-        T temp;
 
-        // Traverse up and fix violated property
-        int current = heapSize;
-        int parent = ((current - 1)/2);
-        while (heap[current].compareTo(heap[parent]) < 0) {
-            temp = heap[current];
-            heap[current] = heap[parent];
-            heap[parent] = temp;
-        }
+    //TODO: double heap size
+    public void maxHeapInsert(T o) {
+
+        heap[heapSize] = o;
+        maxHeapify(0);
         heapSize++;
-        maxHeapify(1);
     }
 
-    public void maxHeapify(int pos){
-        int left = 2*pos;
-        int right = 2*pos+1;
-        int largest;
-        T temp;
-
-        if(left <= heapSize && heap[left].compareTo(heap[pos]) < 0){
-            largest = left;
-        }else{
-            largest = pos;
-        }
-        if (right <= heapSize && heap[right].compareTo(heap[pos]) < 0){
-            largest = right;
-        }
-        if (largest != pos){
-            increaseKey(pos,largest);
-            maxHeapify(largest);
-        }
+    public void maxHeapify(int pos) {
+       int left = left(pos);
+       int right = right(pos);
+       int largest;
+       if (left < heapSize && heap[left].compareTo(heap[pos]) > 0){
+           largest = left;
+       }else{
+           largest = pos;
+       }
+       if (right < heapSize && heap[right].compareTo(heap[pos]) > 0){
+           largest = right;
+       }
+       if (largest != pos){
+           swap(pos, largest);
+           maxHeapify(largest);
+       }
     }
 
     //Use it as a swap function
-    public void increaseKey(int pos1, int pos2){
+    public void increaseKey(int pos1, int pos2) {
         T temp = heap[pos1];
         heap[pos1] = heap[pos2];
         heap[pos2] = temp;
 
     }
 
-    public Boolean isEmpty(){
+    public Boolean isEmpty() {
         return (heapSize == 0);
     }
 
-    public int getSize(){
+    public int getSize() {
         return heapSize;
     }
 
-    public void setSize(int heapSize){
-        this.heapSize  = heapSize;
+    public void setSize(int heapSize) {
+        this.heapSize = heapSize;
     }
 
     public int getCapacity() {
         return capacity;
     }
 
-    public void setCapacity(int capacity){
+    public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
 
-    public T[] getHeap(){
+    public T[] getHeap() {
         return heap;
     }
-}
 
+
+    //TODO: Remove this
+    public void print() {
+
+
+        for (int i = 0; i < heapSize / 2; i++) {
+            int leftChild = left(i);
+            int rightChild = right(i);
+            System.out.print("Parent Node : " + heap[i]);
+
+            if (leftChild < i) //if the child is out of the bound of the array
+                System.out.print(" Left Child Node: " + heap[leftChild]);
+
+            if (rightChild < heapSize) //if the right child index must not be out of the index of the array
+                System.out.print(" Right Child Node: " + heap[rightChild]);
+
+            System.out.println(); //for new line
+
+        }
+
+    }
+}
