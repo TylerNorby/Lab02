865c18637123141ac896e1385d3e2f135f1acf79
Tyler Norby <tyler.norby@live.com>
2022-03-08 19:46:25 -0700

    Lab 02 Finished, calling it here

--- ProcessGenerator.java
+++ ProcessGenerator.java
@@ -1,3 +1,8 @@
+/*
+   Author: Tyler Norby
+   Project: Lab02
+   Description: implementation of ProcessGenerator, fills PQueue table
+    */
 import java.util.Random;
 
 public class ProcessGenerator {
@@ -5,14 +10,18 @@ public class ProcessGenerator {
     private double genProb;
     Random rand = new Random();
 
+    // Default Constructor
     public ProcessGenerator(double genProb) {
         this.genProb = genProb;
     }
 
+
+    // Sends a new query
     public boolean query() {
         return rand.nextDouble() < genProb;
     }
 
+    // Generates new Process for the PQueue
     public Process getNewProcess(int currentTime, int maxProcessTime, int maxLevel) {
         int priority = rand.nextInt(maxLevel) + 1;
         int timeRemaining = rand.nextInt(maxProcessTime) + 1;
